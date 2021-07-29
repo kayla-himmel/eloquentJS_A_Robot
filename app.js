0528cce4812014afdda383613999a8cf34cf4e8e
Kayla Himmelberger <38771553+kaylahimmel@users.noreply.github.com>
2021-07-29 09:45:59 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const arrayOf100 = [];
+
+function makeArrayOf100() {
+    for (let i = 0; i < 100; i++) {
+        let item = i;
+        arrayOf100.push(item);
+    }
+};
+
+// 
+function randomPicker(array) {
+    let choice = Math.floor(Math.random() * array.length);
+    return array[choice];
+};
+
+
+const option1Array = []
+
+
+function compareRobots(robot1, robot1Memory, robot2, robot2Memory) {
+    // empty starting array to hold the generated tasks
+    let tasks = [];
+ 
+
+};
+
+// CALL FUNCTIONS
+// make our array of 100 tasks
+makeArrayOf100();
+
+// call randomPicker robot
+randomPicker(arrayOf100);
+
